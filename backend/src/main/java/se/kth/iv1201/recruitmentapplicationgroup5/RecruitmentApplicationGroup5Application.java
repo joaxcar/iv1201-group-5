71c248eef95ddf73b50e6cb62d5101c924f57c64
nedvin <in.nivde@gmail.com>
2021-02-04 05:40:29 +0100

    Initialize Spring Boot backend
    This commit initializes the backend for the application.
    The backend is initialized with Spring Initializr and includes the
    dependencies Spring Data JPA and Spring Web MVC.

---
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/RecruitmentApplicationGroup5Application.java
@@ -0,0 +1,13 @@
+package se.kth.iv1201.recruitmentapplicationgroup5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentApplicationGroup5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentApplicationGroup5Application.class, args);
+	}
+
+}
