c71f64baf1195c599b54685211c80feca14aac7e
nedvin <in.nivde@gmail.com>
2021-02-05 20:22:38 +0100

    Change to .jsx extension standard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 
+import Registration from "./containers/Registration";
+
 function App() {
 	return (
 		<>
 			<CssBaseline />
-			Recruitment Application UI
+			<Registration />
 		</>
 	);
 }
