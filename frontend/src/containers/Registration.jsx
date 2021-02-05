08f9ae3eb0c2144ac3e446950a5cb8d6ef008ff2
nedvin <in.nivde@gmail.com>
2021-02-05 20:56:10 +0100

    Add form elements

--- frontend/src/containers/Registration.jsx
+++ frontend/src/containers/Registration.jsx
@@ -1,5 +1,39 @@
+import { Container, TextField, Grid, Typography } from "@material-ui/core";
+
 function Registration() {
-	return <>Registration</>;
+	return (
+		<Container>
+			<Typography variant="h4">Register</Typography>
+			<form>
+				<Grid container direction="column" spacing={2}>
+					<Grid item>
+						<TextField label="First name" />
+					</Grid>
+					<Grid item>
+						<TextField label="Last name" />
+					</Grid>
+					<Grid item>
+						<TextField label="E-mail address" />
+					</Grid>
+					<Grid item>
+						<TextField
+							type="date"
+							label="Date of birth"
+							InputLabelProps={{
+								shrink: true,
+							}}
+						/>
+					</Grid>
+					<Grid item>
+						<TextField label="Username" />
+					</Grid>
+					<Grid item>
+						<TextField label="Password" type="password" />
+					</Grid>
+				</Grid>
+			</form>
+		</Container>
+	);
 }
 
 export default Registration;
