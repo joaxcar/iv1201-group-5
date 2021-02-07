cdb4270ab0fa3dbd82cc914e3a37dbe3b738ad73
Richard W <wallin.90@hotmail.com>
2021-02-07 13:21:43 +0100

    Added spring security starter dependency

--- backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/RecruitmentApplicationGroup5Application.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/RecruitmentApplicationGroup5Application.java
@@ -2,8 +2,9 @@ package se.kth.iv1201.recruitmentapplicationgroup5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class RecruitmentApplicationGroup5Application {
 
 	public static void main(String[] args) {
