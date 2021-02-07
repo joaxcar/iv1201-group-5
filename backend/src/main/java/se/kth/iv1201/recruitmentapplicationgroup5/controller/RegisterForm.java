83840acdfefd63109f306837a916a2923a034d43
Johan Carlsson <johacarl@kth.se>
2021-02-07 21:26:37 +0100

    Create registration form and controller

---
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/controller/RegisterForm.java
@@ -0,0 +1,16 @@
+package se.kth.iv1201.recruitmentapplicationgroup5.controller;
+
+public class RegisterForm {
+
+	private String name;
+
+	public RegisterForm(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
