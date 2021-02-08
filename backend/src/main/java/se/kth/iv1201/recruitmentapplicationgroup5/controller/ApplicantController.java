08bc1fa4127009b8ac58ceb9bd3847ed49c2def0
Johan Carlsson <johacarl@kth.se>
2021-02-08 10:31:10 +0100

    Add RegistrationForm as request body

--- backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/controller/ApplicantController.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/controller/ApplicantController.java
@@ -1,27 +1,19 @@
 package se.kth.iv1201.recruitmentapplicationgroup5.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import se.kth.iv1201.recruitmentapplicationgroup5.model.Applicant;
-import se.kth.iv1201.recruitmentapplicationgroup5.model.ApplicantRepository;
-
 @RestController
-@RequestMapping("/api/v1")
+@Validated
 public class ApplicantController {
 
-	private final ApplicantRepository repository;
-
-	ApplicantController(ApplicantRepository repository) {
-		this.repository = repository;
-	}
-
 	@PostMapping("/applicant")
-	Applicant newApplicant(@RequestBody RegisterForm registerForm) {
-		Applicant newApplicant = new Applicant(registerForm.getName());
-		return repository.save(newApplicant);
+	RegistrationForm newApplicant(@Valid @RequestBody RegistrationForm registerForm) {
+		return registerForm;
 	}
 
 }
