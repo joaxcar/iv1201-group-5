de385a1b95c8a462aa7b2d0e2d2585b68347a911
Richard W <wallin.90@hotmail.com>
2021-02-09 10:56:37 +0100

    Add comment to integration layer

--- backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/integration/AccountRepository.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/integration/AccountRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import se.kth.iv1201.recruitmentapplicationgroup5.model.Account;
 
+/**
+ * Interface containing database connection methods for
+ *  {@link se.kth.iv1201.recruitmentapplicationgroup5.model.Account}.
+ * @author Richard
+ *
+ */
 public interface AccountRepository extends CrudRepository<Account, Integer>{
 
 }
--- backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/integration/PersonRepository.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/integration/PersonRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import se.kth.iv1201.recruitmentapplicationgroup5.model.Person;
 
+/**
+ * Interface containing database connection methods for
+ *  {@link se.kth.iv1201.recruitmentapplicationgroup5.model.Person}.
+ * @author Richard
+ *
+ */
 public interface PersonRepository extends CrudRepository<Person, Integer>{
 
 }
