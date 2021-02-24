793454f1b3d652d4bb2620068fde4bf611e3a1d3
nedvin <in.nivde@gmail.com>
2021-02-24 19:51:05 +0100

    Add some colors to show what is what

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,5 +1,16 @@
+import { Typography, Container } from "@material-ui/core";
+import classes from "./footer.module.css";
+
 function Footer() {
-	return <footer>this is the footer</footer>;
+	return (
+		<footer className={classes.footer}>
+			<Container>
+				<Typography variant="h6">
+					Created by Group Five Productions
+				</Typography>
+			</Container>
+		</footer>
+	);
 }
 
 export default Footer;
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,12 @@
-import { Typography } from "@material-ui/core";
+import { Typography, Container } from "@material-ui/core";
+import classes from "./header.module.css";
 
 function Header() {
 	return (
-		<header>
-			<Typography variant="h2">Amusement Park</Typography>
+		<header className={classes.header}>
+			<Container>
+				<Typography variant="h2">Amusement Park</Typography>
+			</Container>
 		</header>
 	);
 }
