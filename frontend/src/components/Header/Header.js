38e14d6e8761dc9a444bc61ea04653798ceca220
nedvin <in.nivde@gmail.com>
2021-02-25 07:17:54 +0100

    Add comments to public definitions

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import { Typography, Container } from "@material-ui/core";
 import classes from "./header.module.css";
 
+/**
+ * The header. To be used on all screens.
+ */
 function Header() {
 	return (
 		<header className={classes.header}>
 			<Container>
-				<Typography variant="h2">Amusement Park</Typography>
+				<Typography variant="h3">Amusement Park</Typography>
 			</Container>
 		</header>
 	);
