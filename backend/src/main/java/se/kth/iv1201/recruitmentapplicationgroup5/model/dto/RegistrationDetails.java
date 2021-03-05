c839f5fdbf17fd129242d96d373b44564b893339
nedvin <in.nivde@gmail.com>
2021-03-05 08:21:19 +0100

    Move date validation to RegistrationDetails

--- backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/model/dto/RegistrationDetails.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/model/dto/RegistrationDetails.java
@@ -1,14 +1,20 @@
 package se.kth.iv1201.recruitmentapplicationgroup5.model.dto;
 
+import java.time.LocalDate;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
-import lombok.Value;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Value
+import lombok.Data;
+
+@Data
 public class RegistrationDetails {
 	@NotNull(message = "Must have field: name")
 	@Valid
@@ -17,11 +23,17 @@ public class RegistrationDetails {
 	@Email(message = "Invalid email format") 
 	String email;
 	@NotNull(message = "Must have field: dateOfBirth")
-	@Valid
-	DateOfBirthDTO dateOfBirth;
+	@Past(message = "dateOfBirth must be a past date")
+	LocalDate dateOfBirth;
 	@NotEmpty(message = "Field username must be atleast one letter")
 	String username;
 	@NotNull(message = "Must have field: password")
 	@Size(min = 8, message = "password must be atleast 8 characters long")
 	String password;
+	
+	@JsonProperty("dateOfBirth")
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = LocalDate.parse(dateOfBirth);
+	}
 }
