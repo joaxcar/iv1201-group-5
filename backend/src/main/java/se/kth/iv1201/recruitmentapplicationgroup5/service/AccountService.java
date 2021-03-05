f7f8fe8c227ef2f4023d42882792b85ae0cbb1e3
nedvin <in.nivde@gmail.com>
2021-03-05 10:14:49 +0100

    WIP

--- backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/service/AccountService.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/service/AccountService.java
@@ -1,14 +1,7 @@
 package se.kth.iv1201.recruitmentapplicationgroup5.service;
 
-import java.time.LocalDate;
-
 import javax.validation.Valid;
 
-import org.modelmapper.AbstractConverter;
-import org.modelmapper.Converter;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -16,8 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import se.kth.iv1201.recruitmentapplicationgroup5.integration.AccountRepository;
 import se.kth.iv1201.recruitmentapplicationgroup5.model.Account;
+import se.kth.iv1201.recruitmentapplicationgroup5.model.FullName;
+import se.kth.iv1201.recruitmentapplicationgroup5.model.Person;
 import se.kth.iv1201.recruitmentapplicationgroup5.model.dto.AccountDTO;
-import se.kth.iv1201.recruitmentapplicationgroup5.model.dto.DateOfBirthDTO;
 import se.kth.iv1201.recruitmentapplicationgroup5.model.dto.RegistrationDetails;
 
 /**
@@ -30,7 +24,7 @@ import se.kth.iv1201.recruitmentapplicationgroup5.model.dto.RegistrationDetails;
 @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
 public class AccountService {
 
-	ModelMapper modelMapper = new ModelMapper();
+//	ModelMapper modelMapper = new ModelMapper();
 
 	@Autowired
 	AccountRepository repository;
@@ -41,12 +35,12 @@ public class AccountService {
 	 * This constructor will configures {@link ModelMapper} to be able to work with
 	 * Lombok value objects.
 	 */
-	public AccountService() {
-		modelMapper.getConfiguration().setFieldMatchingEnabled(true)
-				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE).setMatchingStrategy(MatchingStrategies.LOOSE);
-		modelMapper.createTypeMap(String.class, LocalDate.class);
-		modelMapper.addConverter(stringToDate);
-	}
+//	public AccountService() {
+//		modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+//				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE).setMatchingStrategy(MatchingStrategies.LOOSE);
+//		modelMapper.createTypeMap(String.class, LocalDate.class);
+//		modelMapper.addConverter(stringToDate);
+//	}
 
 	/**
 	 * Add a new account.
@@ -63,21 +57,35 @@ public class AccountService {
 	}
 
 	// Custom converter to get a localdate out of a string with modelmapper
-	private Converter<DateOfBirthDTO, LocalDate> stringToDate = new AbstractConverter<DateOfBirthDTO, LocalDate>() {
-		@Override
-		protected LocalDate convert(DateOfBirthDTO date) {
-			return LocalDate.of(date.getYear(), date.getMonth(), date.getDay());
-		}
-	};
+//	private Converter<DateOfBirthDTO, LocalDate> stringToDate = new AbstractConverter<DateOfBirthDTO, LocalDate>() {
+//		@Override
+//		protected LocalDate convert(DateOfBirthDTO date) {
+//			return LocalDate.of(date.getYear(), date.getMonth(), date.getDay());
+//		}
+//	};
 
 	private Account registrationToAccount(final RegistrationDetails details) {
-		final Account account = modelMapper.map(details, Account.class);
+		FullName fullName = new FullName();
+		fullName.setFirstName(details.getName().getFirst());
+		fullName.setLastName(details.getName().getLast());
+		
+		Person person = new Person();
+		person.setName(fullName);
+		person.setEmail(details.getEmail());
+		person.setBirthDate(details.getDateOfBirth());
+		
+		Account account = new Account();
+		account.setPerson(person);
+		account.setUsername(details.getUsername());
+		account.setPassword(details.getPassword());
+		
 		return account;
 	}
 
 	private AccountDTO accountToDTO(final Account account) {
-		final AccountDTO accountDTO = modelMapper.map(account, AccountDTO.class);
-		return accountDTO;
+		//final AccountDTO accountDTO = modelMapper.map(account, AccountDTO.class);
+		//return accountDTO;
+		return new AccountDTO();
 	}
 
 }
