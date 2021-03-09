bee2fe301c03222e71553de3cd7169017446bc35
nedvin <in.nivde@gmail.com>
2021-03-09 19:41:28 +0100

    Initialize routing in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Layout from "./components/Layout/Layout";
 import RegistrationContainer from "./containers/RegistrationContainer/RegistrationContainer";
 import LoginContainer from "./containers/LoginContainer/LoginContainer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /**
  * Main function of the application
@@ -10,10 +11,21 @@ function App() {
 	return (
 		<>
 			<CssBaseline />
-			<Layout>
-				{/* <RegistrationContainer /> */}
-				<LoginContainer />
-			</Layout>
+			<Router>
+				<Layout>
+					<Switch>
+						<Route exact path="/">
+							<RegistrationContainer />
+						</Route>
+						<Route path="/login">
+							<LoginContainer />
+						</Route>
+						<Route path="/register">
+							<RegistrationContainer />
+						</Route>
+					</Switch>
+				</Layout>
+			</Router>
 		</>
 	);
 }
