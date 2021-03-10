e22359ad677a1e018464e84ed4a88703bdfc9d59
nedvin <in.nivde@gmail.com>
2021-03-10 09:43:04 +0100

    Add profile section of page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,26 @@
+import { useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Cookies from "js-cookie";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect,
+} from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import RegistrationContainer from "./containers/RegistrationContainer/RegistrationContainer";
 import LoginContainer from "./containers/LoginContainer/LoginContainer";
+import ProfileContainer from "./containers/ProfileContainer/ProfileContainer";
 
 /**
  * Main function of the application
  */
-
-const getSession = () => {
-	const jwt = Cookies.get("Authorization");
-	// console.log(jwt);
-	// Cookies.set("foo", "bar");
-	console.log(jwt);
-	let session;
-	try {
-		if (jwt) {
-			const base64Url = jwt.split(".")[1];
-			const base64 = base64Url.replace("-", "+").replace("_", "/");
-			// what is window.atob ?
-			// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/atob
-			session = JSON.parse(window.atob(base64));
-		}
-	} catch (error) {
-		console.log(error);
-	}
-	return session;
-};
-
 function App() {
-	console.log(getSession());
+	const [signedInState, setSignedInState] = useState({
+		isSignedIn: false,
+		accountId: null,
+	});
+
 	return (
 		<>
 			<CssBaseline />
@@ -45,6 +35,13 @@ function App() {
 						</Route>
 						<Route path="/register">
 							<RegistrationContainer />
+						</Route>
+						<Route path="/profile">
+							{signedInState.isSignedIn ? (
+								<ProfileContainer />
+							) : (
+								<Redirect to="/login" />
+							)}
 						</Route>
 					</Switch>
 				</Layout>
---
+++ frontend/src/containers/ProfileContainer/ProfileContainer.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import Profile from "../../components/Profile/Profile";
+import endpoints from "../../properties/endpoints";
+import { postToAPI } from "../../util/network";
+import alertTypes from "../../properties/alerttypes";
+import Alert from "../../components/Alert/Alert";
+
+function formatDetails({
+	firstName,
+	lastName,
+	email,
+	birthYear,
+	birthMonth,
+	birthDay,
+	username,
+	password,
+}) {
+	const dateOfBirth = `${birthYear}-${String(birthMonth).padStart(
+		2,
+		"0"
+	)}-${String(birthDay).padStart(2, "0")}`;
+
+	return {
+		name: {
+			first: firstName,
+			last: lastName,
+		},
+		email: email,
+		dateOfBirth,
+		username: username,
+		password: password,
+	};
+}
+
+/**
+ * Container for Login.
+ */
+function ProfileContainer() {
+	const [alert, setAlert] = useState({ open: false, type: "", message: "" });
+
+	function showAlert(type, message) {
+		setAlert({ open: true, type, message });
+		setTimeout(() => setAlert({ ...alert, open: false }), 7000);
+	}
+
+	function handleFormSubmit(loginDetails, event) {
+		event.preventDefault();
+
+		postToAPI(endpoints.AUTHENTICATE, loginDetails)
+			.then((response) => {
+				event.target.reset();
+				showAlert(alertTypes.SUCCESS, "Login successful");
+			})
+			.catch((error) => {
+				showAlert(
+					alertTypes.ERROR,
+					"Could not login with given credentials"
+				);
+			});
+	}
+
+	return (
+		<>
+			{alert.open ? (
+				<Alert type={alert.type} message={alert.message} />
+			) : null}
+			<Profile onSubmit={handleFormSubmit} />
+		</>
+	);
+}
+
+export default ProfileContainer;
