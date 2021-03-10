c2882546e9d2f90b5595da441b09370496b1256a
nedvin <in.nivde@gmail.com>
2021-03-10 08:23:10 +0100

    WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,36 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Cookies from "js-cookie";
 import Layout from "./components/Layout/Layout";
 import RegistrationContainer from "./containers/RegistrationContainer/RegistrationContainer";
 import LoginContainer from "./containers/LoginContainer/LoginContainer";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /**
  * Main function of the application
  */
+
+const getSession = () => {
+	const jwt = Cookies.get("Authorization");
+	// console.log(jwt);
+	// Cookies.set("foo", "bar");
+	console.log(jwt);
+	let session;
+	try {
+		if (jwt) {
+			const base64Url = jwt.split(".")[1];
+			const base64 = base64Url.replace("-", "+").replace("_", "/");
+			// what is window.atob ?
+			// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/atob
+			session = JSON.parse(window.atob(base64));
+		}
+	} catch (error) {
+		console.log(error);
+	}
+	return session;
+};
+
 function App() {
+	console.log(getSession());
 	return (
 		<>
 			<CssBaseline />
