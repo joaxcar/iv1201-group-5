8fe5e34f9291112c23c07b32de7b808f898e51b4
Johan Carlsson <johacarl@kth.se>
2021-03-12 12:12:37 +0100

    Add javadocs

--- backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/configuration/UserSecurity.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentapplicationgroup5/configuration/UserSecurity.java
@@ -5,12 +5,24 @@ import org.springframework.stereotype.Component;
 
 import se.kth.iv1201.recruitmentapplicationgroup5.model.Account;
 
+/**
+ * Implements security check for users trying to access endpoints
+ *
+ */
 @Component("userSecurity")
 public class UserSecurity {
-     public boolean hasUserId(Authentication authentication, int id) {
-    	 Account account = (Account)authentication.getPrincipal();
-    	 System.out.println(id);
-    	 System.out.println(account.getId());
-    	 return account.getId() == id;
-     }
+
+	/**
+	 * Check if user has access to resource.
+	 * 
+	 * @param authentication current authenticated user for session
+	 * @param id of requesting user
+	 * @return true if user has access
+	 */
+	public boolean hasUserId(Authentication authentication, int id) {
+		Account account = (Account) authentication.getPrincipal();
+		System.out.println(id);
+		System.out.println(account.getId());
+		return account.getId() == id;
+	}
 }
